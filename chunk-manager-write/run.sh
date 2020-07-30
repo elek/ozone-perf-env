536a5490b410e542e731e25ec2959523477612be
Elek Márton <elek@apache.org>
2020-07-30 22:47:37 +0200

    testable chunk manager write test

--- chunk-manager-write/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-kubectl delete -f .
-kubectl wait pod --for=delete -l component=chunk-manager-write
-kubectl wait configmap --for=delete ozone-config
-kubectl wait configmap --for=delete cmw-testscript
-flekszible generate
-kubectl apply -f .
---
+++ chunk-manager-write/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+export K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$K8S_DIR"
+
+# shellcheck source=/dev/null
+source "../testlib.sh"
+
+reset_k8s_env
+
+flekszible generate
+
+kubectl apply -f .
+
+TEST_POD=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l component=env)
+
+MAX_RETRY=100 retry grep_log $TEST_POD "Test is Done"
+
+kubectl logs --tail=20 $TEST_POD > result.txt
