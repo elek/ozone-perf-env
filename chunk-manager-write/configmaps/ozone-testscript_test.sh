959d886ef56faf7937d8a843daef01fc14060a64
Elek Márton <elek@apache.org>
2020-07-30 23:07:47 +0200

    reduce test size

--- chunk-manager-write/configmaps/ozone-testscript_test.sh
+++ chunk-manager-write/configmaps/ozone-testscript_test.sh
@@ -3,7 +3,7 @@
 rm -rf /data/storage/hdds
 time sync
 START_TIME=$(date +%s.%N)
-ozone freon --verbose cmdw -n10000000 -s$(numfmt --from=iec 1K) -t1 -l FILE_PER_BLOCK
+ozone freon --verbose cmdw -n1000000 -s$(numfmt --from=iec 1K) -t1 -l FILE_PER_BLOCK
 time sync
 END_TIME=$(date +%s.%N)
 echo "Total time: $(echo $END_TIME $START_TIME | awk '{print $1 - $2}')"
