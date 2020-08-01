db62be19e06be7e50fdf3367bac16ab07d86cf53
Elek Márton <elek@apache.org>
2020-08-01 14:06:34 +0200

    new version from teragen test

--- teragen/init.sh
+++
@@ -1,2 +0,0 @@
-kubectl exec -it ozone-scm-0 ozone sh volume create  /vol1
-kubectl exec -it ozone-scm-0 ozone sh bucket create  /vol1/bucket1
--- teragen/mr/deploy-test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#set -ex
-kubectl delete -f .
-kubectl delete configmap teragen-testscript
-sleep 10
-kubectl create configmap teragen-testscript --from-file=teragen.sh
-kubectl apply -f .
