f74c757e3da3fc9cd6e9a1ffe999e90e1797b91f
Elek Márton <elek@apache.org>
2020-08-24 16:46:17 +0200

    initial version of flink support
    flink + ozone

---
+++ flink-ozone/run_wordcount.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+kubectl delete job --all
+flekszible generate --print -t namefilter:include=test-runner -t run:args="./bin/flink run ./examples/batch/WordCount.jar --input o3fs://bucket1.vol1/key1 --output o3fs://bucket1.vol1/fileout-$RANDOM" | kubectl apply -f -
+
+kubectl wait --timeout=300s -l job-name=test-runner --for=condition=complete job
+
+kubectl logs --tail=30 -l job-name=test-runner | tee -a results/wordcount.txt
+
+
