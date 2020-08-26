7f0a7ac801466f9bfe9b4c02b4f13586f16a6ce6
Elek Márton <elek@apache.org>
2020-08-26 06:57:27 +0200

    move helper resources to environment subfolder

--- datanode-isolated-follower/configmaps/ozone-testscript_test.sh
+++ datanode-isolated-follower/configmaps/ozone-testscript_test.sh
@@ -2,7 +2,7 @@
 set -ex
 time sync
 START_TIME=$(date +%s.%N)
-ozone freon --verbose falg -n100000 -b1 -s 1024 -c ozone-datanode-0.ozone-datanode:9858 -t1 -r dd54b039-f9fa-409f-bbc5-5dcd2ace838d
+ozone freon --verbose falg -n100000 -b1 -s 1024 -c ozone-datanode-0.ozone-datanode:9858 -t1 -r fb37406e-cfab-4bd7-8211-a6e0842540ae
 time sync
 END_TIME=$(date +%s.%N)
 echo "Total time: $(echo $END_TIME $START_TIME | awk '{print $1 - $2}')"
