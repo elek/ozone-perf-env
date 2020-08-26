735fd94a439d39389a1e191eb2b0afec9174fc6a
Elek Márton <elek@apache.org>
2020-08-26 17:55:08 +0200

    fix parquet tester script

--- spark-ozone/configmaps/testscripts_parquet.sh
+++ spark-ozone/configmaps/testscripts_parquet.sh
@@ -3,12 +3,9 @@
 set -ex
 
 : ${SPARK_HOME:=/opt/spark}
-: ${SOURCE_PATH:=/tmp/qwex}
-: ${DESTINATION_PATH:=/tmp/qwey}
 : ${SAMPLES_DIR:=/opt/}
 
 time $SPARK_HOME/bin/spark-submit \
-    --
     --conf spark.executor.memory=4g \
     --jars /opt/ozonefs/hadoop-ozone-filesystem-hadoop3.jar \
     $SAMPLES_DIR/spark-samples-1.0-SNAPSHOT.jar \
