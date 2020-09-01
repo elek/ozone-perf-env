1c32b9c0fc816bf824b7a8aef806c693fefdb87b
Elek Márton <elek@apache.org>
2020-09-01 20:24:18 +0200

    fix spark runner scripts

--- spark-ozone/run_console.sh
+++ spark-ozone/run_console.sh
@@ -4,7 +4,3 @@ kubectl delete job --all
 
 flekszible generate --print -t namefilter:include=test-runner -t run:args="sleep infinity" | kubectl apply -f -
 
-kubectl wait --timeout=300s -l job-name=test-runner --for=condition=complete job
-
-kubectl logs --tail=30 -l job-name=test-runner | tee -a results/wordcount.txt
-
--- spark-ozone/run_wordcount.sh
+++ spark-ozone/run_wordcount.sh
@@ -2,7 +2,7 @@
 
 kubectl exec ozone-scm-0 -- ozone sh key put /vol1/bucket1/key1 README.md
 
-flekszible generate --print -t namefilter:include=test-runner -t run:args="/opt/spark/bin/run-example --jars /opt/ozonefs/hadoop-ozone-filesystem-hadoop3.jar\,./examples/jars/spark-examples_2.12-3.0.0.jar org.apache.spark.examples.JavaWordCount o3fs://bucket1.vol1/key1" | kubectl apply -f -
+flekszible generate --print -t namefilter:include=test-runner -t run:args="/opt/spark/bin/run-example --jars /opt/ozonefs/hadoop-ozone-filesystem.jar\,./examples/jars/spark-examples_2.12-3.0.0.jar org.apache.spark.examples.JavaWordCount o3fs://bucket1.vol1/key1" | kubectl apply -f -
 
 kubectl wait --timeout=300s -l job-name=test-runner --for=condition=complete job
 
