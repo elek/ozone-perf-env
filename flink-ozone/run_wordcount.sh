099a4227d43fd7ba8faee8d1d8dc5fcbdd0966b8
Elek Márton <elek@apache.org>
2020-09-02 22:31:28 +0200

    fix flink test watcher

--- flink-ozone/run_wordcount.sh
+++ flink-ozone/run_wordcount.sh
@@ -5,7 +5,7 @@ set -ex
 kubectl delete job --all
 flekszible generate --print -t namefilter:include=test-runner -t run:args="./bin/flink run ./examples/batch/WordCount.jar --input o3fs://bucket1.vol1/key1 --output o3fs://bucket1.vol1/fileout-$RANDOM" | kubectl apply -f -
 
-kubectl wait --timeout=300s -l job-name=test-runner --for=condition=complete job
+kubectl wait --timeout=300s -l app=flink,component=test-runner --for=condition=complete job
 
 kubectl logs --tail=30 -l job-name=test-runner | tee -a results/wordcount.txt
 
