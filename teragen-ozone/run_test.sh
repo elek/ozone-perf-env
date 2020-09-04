e8fe9e8421a57ca9ef67f775485ee3901e967d03
Elek Márton <elek@apache.org>
2020-09-04 14:02:33 +0200

    [ozone-teragen] reduce default test size

--- teragen-ozone/run_test.sh
+++ teragen-ozone/run_test.sh
@@ -6,6 +6,7 @@ kubectl delete job --all
 
 flekszible generate \
     -t namefilter:include=test-runner \
+    -t env:TEST_SIZE=${TEST_SIZE:1g} \
     -t env:TEST_MAPPERS=${TEST_MAPPERS:-2} \
     --print | kubectl apply -f -
 
