a8b721d415f5e9973e3cf33698e068759b5b9dd9
Elek Márton <elek@apache.org>
2020-10-02 13:50:46 +0200

    btm files are split

--- spark-ozone/configmaps/testscripts_parquet.sh
+++ spark-ozone/configmaps/testscripts_parquet.sh
@@ -4,9 +4,15 @@ set -ex
 
 : ${SPARK_HOME:=/opt/spark}
 : ${SAMPLES_DIR:=/opt/}
+: ${BTM_SCRIPT:=watchforcommit.btm}
 
+
+#--conf "spark.driver.extraJavaOptions=-javaagent:/opt/byteman/lib/byteman.jar=script:/opt/btm/$BTM_SCRIPT" \
+#-agentpath:/opt/java-async-profiler/build/libasyncProfiler.so=start,file=/tmp/profile-%t-%p.svg
 time $SPARK_HOME/bin/spark-submit \
     --conf spark.executor.memory=4g \
-    --jars /opt/ozonefs/hadoop-ozone-filesystem-hadoop3.jar \
+    --conf "spark.driver.extraJavaOptions=-agentpath:/opt/java-async-profiler/build/libasyncProfiler.so=start,file=/tmp/profile-%t-%p.svg" \
+    --jars /opt/ozonefs/hadoop-ozone-filesystem.jar \
     $SAMPLES_DIR/spark-samples-1.0-SNAPSHOT.jar \
     $@
+sleep infinity
